81832fc5160cd84c89969764499b4759d0a8b943
superk668 <superk977@163.com>
2023-12-15 09:49:47 +0800

    test code

--- main.py
+++ main.py
@@ -7,39 +7,90 @@ import minimatrix1 as mm
 # The following code is only for your reference
 # Please write the test code yourself
 
-mat_a = mm.Matrix([[0,3.0],[2.0,0]])
-print(mat_a)
+#Test __str__
+test = mm.Matrix([[1],[2],[3]])
+print(test)
+
+#Test shape
+test = mm.Matrix([[1,2,3]])
+print(test.shape())
+
+#Test reshape
+test = mm.Matrix([[1,2,3]])
+print(test.reshape((3,1)))
+print(test.reshape((2,5)))
+
+#Test dot
+testa = mm.Matrix([[1],[2],[3]])
+testb = mm.Matrix([[4,5,6]])
+print(testa.dot(testb))
+
+#Test T
+test = mm.Matrix([[1,2,3],[4,5,6]])
+print(test.T())
+
+#Test sum
+test = mm.Matrix([[1,2,3],[4,5,6]])
+print(test.sum(axis=1))
+print(test.sum(axis=0))
+print(test.sum(axis=None))
+
+#Test Kronecker product
+testa = mm.Matrix([[1],[2],[3]])
+testb = mm.Matrix([[4,5,6]])
+print(testa.Kronecker_product(testb))
+
+#Test __getitem__
+test = [[1,2,3],[4,5,6],[7,8,9]]
+print(test[2][1])
+print(test[1][1:3])
+"""print(test[0:2][2])""" #error here
+
+#Test __setitem__
+
+
+
+#Test __pow__
+test = mm.Matrix([[1,2,3],[4,5,6],[7,8,9]])
+print(test**3)
+print((test.dot(test)).dot(test))
+
+#Test __add__ , __sub__ , __mul__ , __len__
+testa = mm.Matrix([[1,2,3],[4,5,6],[7,8,9]])
+testb = mm.Matrix([[9,8,7],[6,5,4],[3,2,1]])
+print(testa + testb)
+print(testa - testb)
+print(testa * testb)
+print(len(testa))
+
+#Test det
+testa = mm.Matrix([[1,2,9],[7,5,15],[4,8,2]])
+print(testa.det())
+testb = mm.Matrix([[0]])
+print(testb.det())
+testc = mm.Matrix([[0],[0],[0]])
+print(testc.det())
+
+#Test inverse
+
+
+#Test rank
+
+#Test I(n)
+print(mm.I(3))
+print(mm.narray((3,5),init_value=2))
+print(mm.arange(2,8,2))
+print(mm.zeros((3,4)))
+print(mm.zeros_like(testa))
+print(mm.ones((3,4)))
+print(mm.ones_like(testa))
+print(mm.nrandom((3,4)))
+print(mm.nrandom_like(testa))
 
 
 
 
-
-
-
-
-'''mat = mm.Matrix([[1,2,3],[6,5,4],[7,8,9]])
-print(mat)
-print(mat.data)
-print(mat.dim)
-print(mat.inverse())
-print(mat.rank())
-print(mat.shape())
-print(mat.T)
-print(mat.det())'''
-
-# m24 = mm.arange(0, 24)
-'''print(m24)
-print(m24.reshape([3, 8]))
-print(m24.reshape([4, 6]))
-'''
-'''print(mm.zeros_like(m24))
-print(mm.ones_like(m24))
-m2 = mm.nrandom((3, 3))
-print(m2)
-print(mm.nrandom_like(m24))
-'''
-
-"""m = 1000
+m = 1000
 n = 100
 X = mm.nrandom((m, n))
 w = mm.nrandom((n, 1))
@@ -49,19 +100,23 @@ e = mm.nrandom((m, 1))
 mean = e.sum()[0, 0] / m
 e -= mm.narray((m, 1), mean)
 Y = X.dot(w) + e
-w_ = X.T().dot(X).inverse().dot(X.T()).dot(Y)"""
+w_ = X.T().dot(X).inverse().dot(X.T()).dot(Y)
 
 
-'''print()
+print()
 print(w.sum())
-print(w_.sum())'''
+print(w_.sum())
 
-"""@mm.vectorize
+@mm.vectorize
 def square(x):
     return x ** 2
 
 mse = square(w - w_).sum()[0, 0] / n
-print(mse)"""
+print(mse)
+
+"""a = mm.Matrix([[1,0,0],[0,1,0],[0,0,1]])
+print(a.rank())"""
+
 
 
 '''@vectorize
