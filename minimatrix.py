ce49c9595b6c87a85711fe1b6fbb90ad3b7c585c
superk668 <superk977@163.com>
2023-12-18 15:10:35 +0800

    gg

--- minimatrix.py
+++ minimatrix.py
@@ -15,12 +15,11 @@ class Matrix:
             self.data = [[init_value for c in range(dim[1])] for r in range(dim[0])]
         else:
             print("你没有提供任何数据！")
-    
-    
+
     def shape(self):
         return self.dim
     
-    def reshape(self, newdim):
+    def reshape(self, newdim):   
         if self.dim[0] * self.dim[1] == newdim[0] * newdim[1]:
             data_lst = [0] * (self.dim[0] * self.dim[1])
             for r in range(self.dim[0]):
@@ -30,12 +29,19 @@ class Matrix:
             return Matrix(ans_lst)
         else:
             print("维数错误")
-            return Matrix([[0]])
+            return 
 
     def dot(self, other):
+        
+        # 注意到在不符合运算规则时，输出为None，在复合运算中可能导致程序报错
+        # 因此在需要的函数开头，先检查所有矩阵参数是否为空
+        # 下同
+        if other == None:
+            return
+        
         if self.dim[1] != other.dim[0]:
             print("维数错误，无法相乘。")
-            return Matrix([[0]])
+            return 
         lst_mat = [[sum(self[r, i] * other [i, c] for i in range(self.dim[1])) 
                     for c in range(other.dim[1])] for r in range(self.dim[0])]
         ans_mat = Matrix(lst_mat)
@@ -48,7 +54,7 @@ class Matrix:
                 T_mat.data[r][c] = self.data[c][r]
         return T_mat
 
-    def sum(self, axis=None):
+    def sum(self, axis=None): 
         if axis == 0:
             return Matrix([[sum([self.data[i][j] for i in range(self.dim[0])]) for j in range(self.dim[1])]])
         if axis == 1:
@@ -56,11 +62,13 @@ class Matrix:
         elif not axis:
             return Matrix([[sum(self.data[i][j] for i in range(self.dim[0]) for j in range(self.dim[1]))]])
 
-    def copy(self):
+    def copy(self):   
         lst = [[c for c in r] for r in self.data]
         return Matrix(lst)
 
     def Kronecker_product(self, other):
+        if other == None:
+            return
         ans_mat = Matrix(dim=(self.dim[0] * other.dim[0], self.dim[1] * other.dim[1]))
         for r in range(self.dim[0]):
             for c in range(self.dim[1]):
@@ -132,7 +140,7 @@ class Matrix:
     def __pow__(self, n):
         if self.dim[0] != self.dim[1]:
             print("矩阵不是方阵。")
-            return Matrix([[0]])
+            return
         else:
             ans = I(self.dim[0])
             n_ = n
@@ -146,27 +154,33 @@ class Matrix:
         return ans
     
     def __add__(self, other):
+        if other == None:
+            return
         if self.dim != other.dim:
             print("矩阵维数不同，无法相加！")
-            return Matrix(dim=(1,1))
+            return
         else:
             ans_lst = [[self.data[r][c] + other.data[r][c] for c in range(self.dim[1])]
                        for r in range(self.dim[0])]
             return Matrix(ans_lst)
 
     def __sub__(self, other):
+        if other == None:
+            return
         if self.dim != other.dim:
             print("矩阵维数不同，无法相减！")
-            return Matrix(dim=(1,1))
+            return
         else:
             ans_lst = [[self.data[r][c] - other.data[r][c] for c in range(self.dim[1])]
                         for r in range(self.dim[0])]
             return Matrix(ans_lst)
 
     def __mul__(self, other):
+        if other == None:
+            return
         if self.dim != other.dim:
             print("矩阵维数不同，无法相乘！")
-            return Matrix(dim=(1,1))
+            return
         else:
             ans_lst = [[self.data[r][c] * other.data[r][c] for c in range(self.dim[1])]
                         for r in range(self.dim[0])]
@@ -205,7 +219,7 @@ class Matrix:
     def det(self):
         if self.dim[0] != self.dim[1]:
             print("三行四列的行列式我没有见过啊！")
-            return 0
+            return
         else:          
             ans = 1            
             ans_mat = self.copy()
@@ -221,7 +235,7 @@ class Matrix:
     def inverse(self):
         if self.dim[0] != self.dim[1]:
             print("该矩阵不是方阵，没有逆矩阵。")
-            return Matrix([[0]])
+            return
         dime = self.dim[0]
         joint_mat = Matrix(dim=(dime , dime* 2))
         joint_mat[:, :dime] = self
@@ -230,7 +244,7 @@ class Matrix:
         joint_mat = joint_mat.Gauss_elimination()[0]
         if not any(x for x in joint_mat.data[dime - 1][:dime]):
             print("该矩阵为奇异阵，没有逆矩阵。")
-            return Matrix([[0]])
+            return
         for i in range(dime - 1, -1, -1):
             unify_factor = 1 / joint_mat[i, i]
             u_mat = Matrix(dim=joint_mat[i, :].dim, init_value=unify_factor)
@@ -277,15 +291,15 @@ class Matrix:
         #检验数据是否正确
         if other.dim[1] != 1 or other.dim[0] != self.dim[0]:
             print("数据错误。")
-            return [[0]]
+            return
         #生成增广矩阵
         b = Matrix([self.data[i]+other.data[i] for i in range(self.dim[0])])
         if self.rank() < b.rank():
             print("无解。")
-            return [[0]]
+            return
         if self.rank() < self.dim[1]:
             print("有无穷多组解。")
-            return [[0]]
+            return
         else:
             return self.inverse().dot(other)
 
@@ -323,41 +337,44 @@ def nrandom_like(matrix):
     return Matrix(lst)
 
 def concatenate(items, axis=0):
-    try:
-        if not items:
-            raise ValueError("缺少拼接对象")
+    if not items:
+        print("缺少拼接对象")
+        return None
+
+    for m in items:
+        if type(m) != Matrix:
+            print("拼接对象不是矩阵")
+            return None
+
+    if axis != 0 and axis != 1:
+        print("无法在此维度上拼接")
+        return None
+
+    if axis == 0:
+        col = items[0].dim[1]
         for m in items:
-            if type(m) != Matrix:
-                raise ValueError("拼接对象不是矩阵")
-        if axis != 0 and axis != 1:
-            raise ValueError("无法在此维度上拼接")
-        if axis == 0:
-            col = items[0].dim[1]
-            for m in items:
-                if m.dim[1] != col:
-                    raise ValueError("矩阵形状不匹配")
-            # 行数增加
-            row_num = items[0].dim[0]
-            ans_mat = zeros((len(items) * row_num, items[0].dim[1]))
-            for i in range(len(items)):
-                ans_mat[i * row_num : (i + 1) * row_num, :] = items[i]
-            return ans_mat
-        
-        if axis == 1:
-            row = items[0].dim[0]
-            for c in items:
-                if c.dim[0] != row:
-                    raise ValueError("矩阵形状不匹配")
-            # 列数增加
-            col_num = items[0].dim[1]
-            ans_mat = zeros((items[0].dim[0], len(items) * col_num))
-            for i in range(len(items)):
-                ans_mat[ : , i * col_num : (i + 1) * col_num] = items[i]
-            return ans_mat
-            
-    except Exception as e:
-        print(e)
-        return Matrix([[0]])
+            if m.dim[1] != col:
+                print("矩阵形状不匹配")
+                return None
+        # 行数增加
+        row_num = items[0].dim[0]
+        ans_mat = zeros((len(items) * row_num, items[0].dim[1]))
+        for i in range(len(items)):
+            ans_mat[i * row_num: (i + 1) * row_num, :] = items[i]
+        return ans_mat
+
+    if axis == 1:
+        row = items[0].dim[0]
+        for c in items:
+            if c.dim[0] != row:
+                print("矩阵形状不匹配")
+                return None
+        # 列数增加
+        col_num = items[0].dim[1]
+        ans_mat = zeros((items[0].dim[0], len(items) * col_num))
+        for i in range(len(items)):
+            ans_mat[:, i * col_num: (i + 1) * col_num] = items[i]
+        return ans_mat
 
 def vectorize(func):
     def v_func(mat):
@@ -376,5 +393,3 @@ def vectorize(func):
 if __name__ == "__main__":
     print("test here")   
     # All test code are presented in main.py
-    m1 = Matrix([[0,2,3],[2,3,0],[3,0,4]])
-    print(m1*m1)
